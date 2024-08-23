a1d85cd03e61fd62935fd41a7c81a2e62ffbef5f
gtgopinath5 <gtgopinath5@gmail.com>
2024-08-23 17:34:42 +0530

    commit

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.head('/api', (req, res) => {
-    res.send('Welcome to the Job Portal API');
+    res.send({success:'Welcome to the Job Portal API'});
 });
 //define the environment
 app.use("/api/users", userRouter);
